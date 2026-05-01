fce9bf7275a3117dcfc187f7e2d355ba4e4f1b76
sansii44 <laurainemendoza.ustp@gmail.com>
2026-05-01 16:13:30 +0800

    commit part 2

--- personaldiary.py
+++ personaldiary.py
@@ -1,15 +1,15 @@
 try: 
-    with open("Diary System.txt", "x") as filee:
+    with open("Diary System.txt", "x") as file:
         pass
     print('Diary created')
 
-    with open("Diary System.txt", "w") as filee:
-        filee.write("My Diary")
+    with open("Diary System.txt", "w") as file:
+        file.write("My Diary")
 
     print("File already exists")
 
     entry = input("What do you want to write? ")
 
-    with open("Diary System", "a") as filee: 
+    with open("Diary System", "a") as file: 
         
-except FileExistsError: ajfkakvja
+except FileExistsError: pangit charys
