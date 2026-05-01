abb7320fb24664136a7787fc2494e48437349c23
chacharys12 <mondelo.charys04@gmail.com>
2026-05-01 16:32:20 +0800

    initializes system

--- personaldiary.py
+++ personaldiary.py
@@ -4,7 +4,7 @@ try:
     print('Diary created')
 
     with open("Diary System.txt", "w") as file:
-        file.write(input("What do you want to write?"))
+        file.write(input("What do you want to write??"))
         
 except FileExistsError:
     print ("File already exists")
