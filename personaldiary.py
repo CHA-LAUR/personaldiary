eb78aac74b18f28c5209d48e4812b0721642f073
sansii44 <laurainemendoza.ustp@gmail.com>
2026-05-01 16:09:38 +0800

    commit test 101

--- personaldiary.py
+++ personaldiary.py
@@ -1,1 +1,15 @@
-test file 101
+try: 
+    with open("Diary System.txt", "x") as filee:
+        pass
+    print('Diary created')
+
+    with open("Diary System.txt", "w") as filee:
+        filee.write("My Diary")
+
+    print("File already exists")
+
+    entry = input("What do you want to write? ")
+
+    with open("Diary System", "a") as filee: 
+        
+except FileExistsError: ajfkakvja
